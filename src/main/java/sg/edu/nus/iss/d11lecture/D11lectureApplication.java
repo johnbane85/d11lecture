74490d07b0b9e160079f945297d7d858638b0fce
johnbane85 <junyu.siew@gmail.com>
2022-07-18 12:02:07 +0800

    2nd commit

--- src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
+++ src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.d11lecture;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class D11lectureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11lectureApplication.class, args);
+		// SpringApplication.run(D11lectureApplication.class, args);
+		SpringApplication app = new SpringApplication(D11lectureApplication.class);
+
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+
 	}
 
 }
